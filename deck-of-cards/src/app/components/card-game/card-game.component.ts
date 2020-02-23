ff3e95340e07e5fde18a0e5fe21ac3f462e90006
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-23 23:20:08 +0200

    - displayed cards drawn

--- deck-of-cards/src/app/components/card-game/card-game.component.ts
+++ deck-of-cards/src/app/components/card-game/card-game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { MatDialogRef, MatDialog} from '@angular/material/dialog';
 import { DeckOfCardsApiService } from '../../services/deck-of-cards-api.service';
 import { MatSliderChange } from '@angular/material/slider';
+import { CardResponse } from '../../interfaces/card-response';
 
 @Component({
   selector: 'app-card-game',
@@ -11,6 +12,7 @@ import { MatSliderChange } from '@angular/material/slider';
 export class CardGameComponent implements OnInit {
   private currentDeckId: string | null = null;
   private nrOfCardsToDraw: number = 2;
+  private cardsDrawn: CardResponse[] | null = null;
 
   public isDeckDrawn(): boolean {
     return this.currentDeckId != null && this.currentDeckId.length > 0;
@@ -24,11 +26,19 @@ export class CardGameComponent implements OnInit {
   }
 
   refreshDeck() {
-    this.apiService.brandNewDeck().subscribe(response => { this.currentDeckId = response.deck_id; this.openDialog(); })
+    this.apiService.brandNewDeck().subscribe(response => {
+      this.currentDeckId = response.deck_id;
+      this.openDialog();
+      this.cardsDrawn = null;
+    })
   }
 
   drawCards() {
-    this.apiService.drawACard(this.currentDeckId, this.nrOfCardsToDraw).subscribe(response => console.info(response));
+    this.apiService.drawACard(this.currentDeckId, this.nrOfCardsToDraw).subscribe(response =>
+    {
+      console.info(response);
+      this.cardsDrawn = response.cards;
+    });
   }
 
 
